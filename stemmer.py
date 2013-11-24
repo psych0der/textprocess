15344ae573d4fc0fda477eaddbe3d854bd8106fa
psych0der <mayankbhola@gmail.com>
2013-11-24 20:35:13 +0530

    helper functions added

--- stemmer.py
+++ stemmer.py
@@ -30,3 +30,34 @@ def measure(word):
 	return m
 
 #print measure("aecouciklo")
+
+def containsVowel(word):
+	for index in range(len(word)-1):
+		if not isConsonant(word,index):
+			return True
+		
+	return False
+
+def endsWithDoubleConsonant(word):
+	length = len(word)
+	if isConsonant(word,length-1) and isConsonant(word,length-2):
+		return True
+	else:
+		return False
+
+def cvc(word):
+	length = len(word)
+	if isConsonant(word,length-3):
+		if not isConsonant(word,length-2):
+			if isConsonant(word,length-1) and (word[length-1] not in ['w','x','y']):
+				return True
+			else : 
+				return False
+		else :
+			return False
+	else :
+		return False
+
+
+
+
